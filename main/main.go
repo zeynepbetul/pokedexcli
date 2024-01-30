353cd11be5e5abac3d082bf7faf91182c3a59c3f
zeynepbetul <z.beetulkaya@gmail.com>
2024-01-30 16:03:05 +0300

    Fix wrong import

--- main/main.go
+++ main/main.go
@@ -1,9 +1,9 @@
 package main
 
-import "github.com/zeynepbetul/pokedexcli/internal"
+import "github.com/zeynepbetul/pokedexcli/internal/pokeapi"
 
 func main() { // go build && ./pokedexcli
 	for {
-		internal.StartRepl()
+		pokeapi.StartRepl()
 	}
 }
