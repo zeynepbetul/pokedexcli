9a7302372fcd51818c566db55ea1fa08af0b4ff6
zeynepbetul <z.beetulkaya@gmail.com>
2024-02-26 09:39:35 +0300

    Finish test reap loop

--- tests/cache_test.go
+++ tests/cache_test.go
@@ -9,25 +9,29 @@ import (
 // Test Reap Loop
 // Reap Loop deletes entries which entries are higher than an interval
 func TestReapLoop(t *testing.T) {
-	const interval = 35 * time.Second
+	const interval = 5 * time.Second
 
 	TestEntry := pokecache.CacheEntry{
 		CreatedAt: time.Now(),
-		Val:       []byte{78, 111, 116, 32, 70, 111, 117, 110, 100},
-	}
-	TestEntryToDelete := pokecache.CacheEntry{
-		CreatedAt: time.Now().Add(35 * time.Second),
 		Val:       []byte{78, 111, 116, 32, 70, 111, 117, 110, 100},
 	}
 
 	TestCacheEntries := make(map[string]pokecache.CacheEntry)
 
 	TestCacheEntries["https://pokeapi.co/api/v2/location-area/"] = TestEntry
-	TestCacheEntries["https://pokeapi.co/api/v2/location-area/asd"] = TestEntryToDelete
 
 	TestCache := pokecache.Cache{
 		CacheEntries: TestCacheEntries,
 		Interval:     interval,
 	}
-	TestCache.ReapLoop()
+	_, ok := TestCache.Get("https://pokeapi.co/api/v2/location-area/")
+	if !ok {
+		t.Errorf("expected to find cache entry")
+	}
+	time.Sleep(interval)
+	TestCache.ReapLoop() //
+	_, ok = TestCache.Get("https://pokeapi.co/api/v2/location-area/")
+	if ok {
+		t.Errorf("expected to not find cache entry")
+	}
 }
